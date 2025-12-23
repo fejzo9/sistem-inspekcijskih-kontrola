d267e671838f50d61867acaa407943ff7c0ca9c4
fejzo9 <fejzo999@gmail.com>
2025-12-24 00:04:44 +0100

    Create service InspekcijskaKontrolaService for communicating with /inspekcijske-kontrole endpoint

---
+++ frontend/src/services/InspekcijskaKontrolaService.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+import type { InspekcijskaKontrola } from "../types/inspekcijska-kontrola";
+
+const API_URL = "http://localhost:8080/inspekcijske-kontrole";
+
+const getAll = () => {
+    return axios.get<InspekcijskaKontrola[]>(API_URL, { headers: authHeader() });
+};
+
+const getById = (id: number) => {
+    return axios.get<InspekcijskaKontrola>(`${API_URL}/${id}`, { headers: authHeader() });
+};
+
+const create = (data: InspekcijskaKontrola) => {
+    return axios.post<InspekcijskaKontrola>(API_URL, data, { headers: authHeader() });
+};
+
+const update = (id: number, data: InspekcijskaKontrola) => {
+    return axios.put<InspekcijskaKontrola>(`${API_URL}/${id}`, data, { headers: authHeader() });
+};
+
+const remove = (id: number) => {
+    return axios.delete(`${API_URL}/${id}`, { headers: authHeader() });
+};
+
+const getByPeriod = (startDatum: string, endDatum: string) => {
+    return axios.get<InspekcijskaKontrola[]>(`${API_URL}/period`, {
+        params: { startDatum, endDatum },
+        headers: authHeader(),
+    });
+};
+
+const getByTijelo = (tijeloId: number) => {
+    return axios.get<InspekcijskaKontrola[]>(`${API_URL}/tijelo/${tijeloId}`, { headers: authHeader() });
+};
+
+const getByProizvod = (proizvodId: number) => {
+    return axios.get<InspekcijskaKontrola[]>(`${API_URL}/proizvod/${proizvodId}`, { headers: authHeader() });
+};
+
+const InspekcijskaKontrolaService = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove,
+    getByPeriod,
+    getByTijelo,
+    getByProizvod,
+};
+
+export default InspekcijskaKontrolaService;
