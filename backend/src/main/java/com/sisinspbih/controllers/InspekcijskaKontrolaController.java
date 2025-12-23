69d1b4345d87e4faf164345d1ed19fa3b9177ea0
fejzo9 <fejzo999@gmail.com>
2025-12-23 19:33:50 +0100

    Change API for InspekcijskaKontrola and Test controllers

--- backend/src/main/java/com/sisinspbih/controllers/InspekcijskaKontrolaController.java
+++ backend/src/main/java/com/sisinspbih/controllers/InspekcijskaKontrolaController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/inspekcijske-kontrole")
+@RequestMapping("/inspekcijske-kontrole")
 @CrossOrigin(origins = "*")
 public class InspekcijskaKontrolaController {
 
